07885db44a710ff0eabf01b47bdd8ab39cd089b2
haku72 <roy@burgerdigital.com.au>
2019-12-02 16:37:50 +1000

    add more sizes for icon images

--- src/components/Helmet.jsx
+++ src/components/Helmet.jsx
@@ -3,6 +3,8 @@ import Helmet from 'react-helmet'
 
 import appleTouchIcon from '../images/apple-touch-icon.png'
 import favicon from '../images/favicon.png'
+import favicon16 from '../images/favicon-16x16.png'
+import favicon32 from '../images/favicon-32x32.png'
 import safariPinnedTab from '../images/safari-pinned-tab.svg'
 
 const HelmetMeta = ({title}) => {
@@ -10,7 +12,9 @@ const HelmetMeta = ({title}) => {
         <Helmet>
             <title>Simon Buutveld - {title}</title>
             <link rel="shortcut icon" type="image/png" href={favicon} />
-            <link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
+            <link rel="icon" type="image/png" href={favicon16} sizes="16x16" />
+            <link rel="icon" type="image/png" href={favicon32} sizes="32x32" />
+            <link rel="apple-touch-icon" type="image/png" sizes="180x180" href={appleTouchIcon} />
             <link rel="manifest" href="./manifest.json" />
             <link rel="mask-icon" href={safariPinnedTab} color="#f03f36" />
             <meta name="msapplication-TileColor" content="#f03f36" />
