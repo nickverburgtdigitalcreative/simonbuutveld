177a702236d30a8ad80c5f2baca3f79269ed65d9
haku72 <roy@burgerdigital.com.au>
2019-12-10 17:07:36 +1000

    up version number for cache bust

--- src/components/Helmet.jsx
+++ src/components/Helmet.jsx
@@ -9,9 +9,9 @@ const HelmetMeta = ({title}) => {
     return (
         <Helmet>
             <title>Simon Buutveld - {title}</title>
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=2" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=2" />
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=2" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=2.1" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=2.1" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=2.1" />
             <link rel="manifest" href="./manifest.json" />
             <link rel="mask-icon" href="/safari-pinned-tab.svg?v=2" color="#f03f36" />
             <meta name="msapplication-TileColor" content="#f03f36" />
